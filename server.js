3892dabbd0e37008f890fa4b4308bbf60bf01859
sam <samuel.j.simmons@gmail.com>
2016-04-08 08:47:10 +1200

    add callback functions before replying

--- server.js
+++ server.js
@@ -45,40 +45,49 @@ server.route({
 				//TEST check whether needs parsing
 				var playerCard = JSON.parse(request.payload)
 
-				console.log('computerCard: ', computerCard.rating, typeof computerCard.rating)
-				console.log('playercard: ', playerCard.rating, typeof playerCard.rating)
-
 				if (parseInt(computerCard.rating) > parseInt(playerCard.rating)) {
 					currentScore.p2 += 1
 				}
 				else {
 					currentScore.p1 += 1
 				}
-				//TODO add callbacks for savescore and savecomputerdeck and nest reply inside there
-				saveScore(JSON.stringify(currentScore))
-				saveComputerDeck(JSON.stringify(computerDeck))
-				reply(currentScore)
+				//TEST add callbacks for savescore and savecomputerdeck and nest reply inside there
+				// saveScore(JSON.stringify(currentScore), function ())
+				// saveComputerDeck(JSON.stringify(computerDeck))
+				// reply(currentScore)
+				fs.writeFile('./data/score.json', (err) => {
+					if (err) {
+						throw err
+					}
+					fs.writeFile('./data.computerdeck.json', (err) => {
+						if (err) {
+							throw err
+						}
+						reply(currentScore)
+					})
+				})
 			})
 		})		
 
 	}
 })
 
-function saveScore (data) {
-	fs.writeFile('./data/score.json', data, (err) => {
-		if (err) {
-			console.error(err)
-		}
-	})
-}
+// function saveScore (data, callback) {
+// 	fs.writeFile('./data/score.json', data, (err) => {
+// 		if (err) {
+// 			callback(err)
+// 		}
+// 		callback(null, data)
+// 	})
+// }
 
-function saveComputerDeck (data) {
-	fs.writeFile('./data/computerdeck.json', data, (err) => {
-		if (err) {
-			console.error(err)
-		}
-	})
-}
+// function saveComputerDeck (data) {
+// 	fs.writeFile('./data/computerdeck.json', data, (err) => {
+// 		if (err) {
+// 			console.error(err)
+// 		}
+// 	})
+// }
 
 server.route({
 	method: 'GET',
