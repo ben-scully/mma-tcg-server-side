a9e5e7852381d172688d82204917cebff09314c6
sam <samuel.j.simmons@gmail.com>
2016-04-07 15:50:30 +1200

    add data

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const fs = require('fs')
 const Hapi = require('hapi')
 
 const server = new Hapi.Server()
-server.connection({ port: 8000 })
+server.connection(
+	{ 
+		port: 8000,
+		routes: { cors: true }
+	}
+)
 
 server.start((err) => {
 	if (err) {
@@ -14,8 +19,47 @@ server.start((err) => {
 
 server.route({
 	method: 'GET',
+	path: '/new',
+	handler: (request, reply) => {
+		fs.readFile('./deck.json', (err, data) => {
+			if (err) {
+				throw err
+			}
+			reply(data)
+		})
+		
+	}
+})
+
+server.route({
+	method: 'POST',
+	path: '/round',
+	handler: (request, reply) => {
+		fs.readFile('./deck.json', (err, data) => {
+			var roundObj = {
+				"playerOne": 0,
+				"playerTwo": 0,
+				"win": false
+			}
+			console.log(request.payload)
+			//var playerCard = payload something
+			var computerDeck = data.array
+			var computerCard = computerDeck.pop()
+			if (computerCard.rating > playerCard.rating) {
+				roundObj.playerOne += 1
+			}
+			else {
+				roundObj.playerTwo += 1
+			}
+			reply(roundObj)
+		})		
+	}
+})
+
+server.route({
+	method: 'GET',
 	path: '/',
-	handler: (res, reply) => {
+	handler: (request, reply) => {
 		reply('hellllllllo')
 	}
 })
