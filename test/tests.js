ec3f988dbd0d5cc8b7ea026c8b1987951904055e
tony-luisi <t.luisi@gmail.com>
2016-04-08 13:53:31 +1200

    tests working

--- test/tests.js
+++ test/tests.js
@@ -3,8 +3,9 @@ var Hapi = require('hapi')
 var assert = require('chai').assert
 var should = require('chai').should()
 var fs = require('fs')
-
 var server = require('../server')
+
+var expectedDeckJson = fs.readFileSync('./data/playerDeck.json', 'utf8')
 
 describe("Server Initiation", function(){
   it('Root server can be accessed', function (done) {
@@ -25,64 +26,60 @@ describe("Server Initiation", function(){
 describe('New Game', function(){
   describe('Get initial deck', function(){
     it('should return a deck array with 3 cards', function(done){
-      var expectedDeckJson = fs.readFileSync('./data/playerDeck.json', 'utf8')
-      hapiTest({server: server})
-        .get('/new')
-        .end(function(result){
+        getNewDeck(function(result){
           assert.deepEqual(result.payload, expectedDeckJson, 'Api call to /new gives a 3 card deck')
           done()
         })
     })
     it('should return a deck array of 3 cards even if we take a card already', function(done){
-      hapiTest({server: server})
-        .post('/round', {"name": "Fighter One", "img": "test.jpg" ,"rating": 100})
-        .end(function(result){
-          var expectedDeckJson = fs.readFileSync('./data/playerDeck.json', 'utf8')
-          hapiTest({server: server})
-            .get('/new')
-            .end(function(result){
-              assert.deepEqual(result.payload, expectedDeckJson, 'Api call to /new gives a 3 card deck')
-              done()
-            })
+      postCard(function(scoreObject){
+        getNewDeck(function(deckObject){
+          assert.deepEqual(deckObject.payload, expectedDeckJson, 'Api call to /new gives a 3 card deck')
+          done()
         })
+      })
     })
   })
 })
-
 
 describe('Round', function(){
   describe('One round', function(){
     it('should take a card and return a bout score', function(done){
-      var scoreObject = {name: 'Fighter One', img: 'test.jpg' ,rating: 100}
-      var stringifiedScoreObject = JSON.stringify(scoreObject)
-      console.log('stringify', stringifiedScoreObject)
+      var scoreObject = JSON.stringify({name: 'Fighter One', img: 'test.jpg' ,rating: '100'})
       hapiTest({server: server})
-        .post('/round', stringifiedScoreObject)
+        .post('/round', scoreObject)
         .end(function(result){
           var scoreObject = JSON.parse(result.payload)
-          console.log('ouput', scoreObject)
-          //assert.notEqual(Object.keys(scoreObject).indexOf('p1'), -1 ,'player one score in the score object exists')
-          //assert.notEqual(Object.keys(scoreObject).indexOf('p2'), -1 ,'player two score in the score object exists')
+          assert.notEqual(Object.keys(scoreObject).indexOf('p1'), -1 ,'player one score in the score object exists')
+          assert.notEqual(Object.keys(scoreObject).indexOf('p2'), -1 ,'player two score in the score object exists')
           done()
         })
     })
-    it('it should return -1 if the card is invalid', function(done) {
+    it('it should return error if the card sent is an empty object', function(done) {
       hapiTest({server: server})
         .post('/round', {})
         .end(function(result){
-          //var scoreObject = JSON.parse(result.payload)
-          //console.log('scoreobject', scoreObject)
-          //var score = scoreObject.playerOne + scoreObject.playerTwo
-          //console.log("SCORE", score)
-          //assert.equal(score, 1)
+          assert.equal(result.payload, "Err: Not a valid card", 'sending an empty object returns an error')
           done()
         })
     })
   })
 })
 
-describe('Bout', function(){
-  describe('One bout', function(){
-    it('should return a winner when the bout is complete')
-  })
-})
+function getNewDeck(callback){
+  hapiTest({server: server})
+    .get('/new')
+    .end(function(result){
+      callback(result)
+    })
+}
+
+function postCard(callback){
+  var scoreObject = JSON.stringify({name: 'Fighter One', img: 'test.jpg' ,rating: '100'})
+  //console.log('stringify', scoreObject)
+  hapiTest({server: server})
+    .post('/round', scoreObject)
+    .end(function(result){
+      callback(result)
+    })
+}
